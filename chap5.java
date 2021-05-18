81d437316fdb14718b56577295c5e86057479891
chomming <81208053+chomming@users.noreply.github.com>
2021-05-18 19:47:40 +0900

    Create chap5.java

---
+++ chap5.java
@@ -0,0 +1,18 @@
+class Student 
+{
+	int age = 20;
+	String name;
+	public void printAge1() {
+		int age = 10;
+		System.out.println("나이 : " + age);
+	}
+	public void printAge2() {
+		System.out.println("나이 : " + age);
+	}
+	public static void main(String[] args)
+	{
+		Student st1 = new Student();
+		st1.printAge1();
+		st1.printAge2();
+	}
+}
