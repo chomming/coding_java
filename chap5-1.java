79bb8ffde884e81674ea80c2f032275c221cf422
chomming <81208053+chomming@users.noreply.github.com>
2021-05-18 19:53:39 +0900

    Update chap5-1.java

--- chap5-1.java
+++ chap5-1.java
@@ -2,13 +2,16 @@ class Student
 {
 	int age = 20;
 	String name;
+	
 	public void printAge1() {
 		int age = 10;
 		System.out.println("나이 : " + age);
 	}
+	
 	public void printAge2() {
 		System.out.println("나이 : " + age);
 	}
+	
 	public static void main(String[] args)
 	{
 		Student st1 = new Student();
